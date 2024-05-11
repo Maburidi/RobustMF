ca2b842af7a02c76560a596f7fc7ccc86dc5e3f8
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-05-11 12:21:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,39 @@ from sklearn.metrics import f1_score
 
 
 from solverFM import *
+from Datasets import Dataset, PrePtbDataset  
+from defense import * 
+from defense import GCN 
+
 
 
 #from deeprobust.graph.defense import GCN
 #from deeprobust.graph.data import Dataset, PrePtbDataset
 #from deeprobust.graph.utils import preprocess, encode_onehot, get_train_val_test
 
+def Parser():
+    parser = argparse.ArgumentParser(description="Argument parser for custom options")
+    parser.add_argument('--no_cuda', type=int, default=0, help="Whether to disable CUDA (0 for False, 1 for True)")
+    parser.add_argument('--ptb_rate', type=float, default=0.1, help="Rate of perturbation for the PTB attack")
+    parser.add_argument('--seed', type=int, default=15, help="Seed for random number generation")
+    parser.add_argument('--dataset', type=str, default='citeseer', help="Name of the dataset to use")
+    parser.add_argument('--attack', type=str, default='meta', choices=['no', 'random', 'meta', 'nettack'],
+                        help="Type of attack to perform")
+    parser.add_argument('--hidden', type=int, default=16, help="Number of hidden units in the model")
+    parser.add_argument('--dropout', type=float, default=0.5, help="Dropout rate for regularization")
+    parser.add_argument('--epochs', type=int, default=250, help="Number of training epochs")
+    parser.add_argument('--only_gcn', action='store_true', help="Whether to use only GCN layer or full model")
+
+    return parser
+
+def main(args):
+   
+
+
+if __name__ == "__main__":
+    parser = Parser()
+    args = parser.parse_args()
+    main(args)
 
 
 
