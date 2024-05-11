fed83bea957d47a130bb6629c970b39087fd19be
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-05-11 12:34:18 -0700

    Update

--- main.py
+++ main.py
@@ -34,20 +34,19 @@ from solverFM import *
 from Datasets import Dataset, PrePtbDataset  
 from defense import * 
 from defense import GCN 
-
-
+from utils import * 
 
 #from deeprobust.graph.defense import GCN
 #from deeprobust.graph.data import Dataset, PrePtbDataset
 #from deeprobust.graph.utils import preprocess, encode_onehot, get_train_val_test
 
 def Parser():
-    parser = argparse.ArgumentParser(description="Argument parser for custom options")
+    parser = argparse.ArgumentParser(description="RobustFM")
     parser.add_argument('--no_cuda', type=int, default=0, help="Whether to disable CUDA (0 for False, 1 for True)")
     parser.add_argument('--ptb_rate', type=float, default=0.1, help="Rate of perturbation for the PTB attack")
     parser.add_argument('--seed', type=int, default=15, help="Seed for random number generation")
-    parser.add_argument('--dataset', type=str, default='citeseer', help="Name of the dataset to use")
-    parser.add_argument('--attack', type=str, default='meta', choices=['no', 'random', 'meta', 'nettack'],
+    parser.add_argument('--dataset', type=str, default='citeseer', help="Name of the dataset to use, 'cora', 'citeseer'")
+    parser.add_argument('--attack', type=str, default='meta', choices=['random','meta','nettack'],
                         help="Type of attack to perform")
     parser.add_argument('--hidden', type=int, default=16, help="Number of hidden units in the model")
     parser.add_argument('--dropout', type=float, default=0.5, help="Dropout rate for regularization")
@@ -57,9 +56,70 @@ def Parser():
     return parser
 
 def main(args):
-   
+    no_cuda = args.no_cuda
+    ptb_rate = args.ptb_rate
+    seed = args.seed
+    dataset = args.dataset
+    attack = args.attack   #random, meta, nettack
+    hidden = args.hidden
+    dropout = args.dropout
+    epochs =  args.epochs
+    only_gcn= only_gcn.args
+
+    cuda = not no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if cuda else "cpu")
+    if cuda:
+        torch.cuda.manual_seed(seed)
+    if ptb_rate == 0:
+        attack = "no"
+
+    ############### DATA - import data ##############
+    data = Dataset(root='/tmp/', name=dataset, setting='prognn')
+    adj, features, labels = data.adj, data.features, data.labels
+    idx_train, idx_val, idx_test = data.idx_train, data.idx_val, data.idx_test
+
+    if dataset == 'pubmed':
+        print("just for matching the results in the paper," + "see details in https://github.com/ChandlerBang/Pro-GNN/issues/2")
+        idx_train, idx_val, idx_test = get_train_val_test(adj.shape[0],val_size=0.1, test_size=0.8, stratify=encode_onehot(labels), seed=15)
+
+    if attack == 'no':
+        perturbed_adj = adj
+
+    labels = torch.LongTensor(labels)
+    features = torch.FloatTensor(np.array(features.todense()))
+
+    ############### Perturb the data - Attach it either randomly, or using meta or nettach ##############
+    if attack == 'random':
+        from deeprobust.graph.global_attack import Random
+        import random; random.seed(seed)
+        np.random.seed(seed)
+        attacker = Random()
+        n_perturbations = int(ptb_rate * (adj.sum()//2))
+        attacker.attack(adj, n_perturbations, type='add')
+        perturbed_adj = attacker.modified_adj
+
+    if attack == 'meta' or attack == 'nettack':
+        perturbed_data = PrePtbDataset(root='/tmp/',name=dataset,attack_method=attack,ptb_rate=ptb_rate)
+        perturbed_adj = perturbed_data.adj
+        if attack == 'nettack':
+            idx_test = perturbed_data.target_nodes
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+
+
+    model = GCN(nfeat=features.shape[1],
+            nhid=hidden,
+            nclass=labels.max().item() + 1,
+            dropout=dropout, device=device)
+
+
+
+
  
 
+   
 if __name__ == "__main__":
     parser = Parser()
     args = parser.parse_args()
